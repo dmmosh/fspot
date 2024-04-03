d86041a5aed4690a1f93a30d2489f3908c75409c
wettestsock <evilsharkeater@gmail.com>
2024-04-03 17:55:28 -0400

    nothing of note

--- src/auth.py
+++ src/auth.py
@@ -10,3 +10,8 @@ CLIENT_SECRET = open("./client_secret.txt", "r").readline() # DEBUG PURPOSES
 REDIRECT_URI = 'http://localhost:5000/callback'
 
 
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+API_BASE_URL = 'https://api.spotify.com/v1/'
+
+
