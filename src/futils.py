14869decea1237a59650c4c34cf95364985ff42f
wettestsock <evilsharkeater@gmail.com>
2024-04-17 21:23:12 -0400

    nothing of note

--- src/futils.py
+++ src/futils.py
@@ -69,10 +69,10 @@ def DELETE(where_from:str, params:dict = None, data:dict = None, json:dict = Non
                             headers=HEADER(headers), 
                             allow_redirects=True)
 
-def loading_msg(process:threading.Thread, msg:str = 'Loading')-> None:
+def loading_msg(process:threading.Thread, msg:str = 'Loading ')-> None:
     while(process.is_alive()):
-        for char in ['.', '..', '...']:
-            sys.stdout.write('\r'+msg+char+'\r')
+        for char in "/—\|":
+            sys.stdout.write('\r'+msg+char)
             time.sleep(.1)
             sys.stdout.flush() 
     print("\r")
