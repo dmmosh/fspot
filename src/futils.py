e160fae5ec4c512c85374ba6dcef2fabee8c8ecc
wettestsock <evilsharkeater@gmail.com>
2024-04-19 15:49:34 -0400

    nothing of note

--- src/futils.py
+++ src/futils.py
@@ -12,7 +12,7 @@ def browser_localhost():
     #webbrowser.open('http://127.0.0.1:5000?access_token=' + gl.auth_codes['access_token'])
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #result = sock.connect_ex(('localhost',5000))
-    if sock.connect_ex(('localhost',5000)): # checks if localhost port is open or not
+    if sock.connect_ex(('127.0.0.1',5000)): # checks if localhost port is open or not
         ERROR('Localhost post is not open.', 'Make sure to save anything that\'s running on it and run \'kill -9 $(lsof -t -i:5000)\'.')
     sock.close()
     webbrowser.open('http://localhost:5000')
