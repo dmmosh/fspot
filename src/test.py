3428c4cff30e0e29e27698d7a55a755594a3a9a4
wettestsock <evilsharkeater@gmail.com>
2024-04-21 01:26:02 -0400

    nothing of note

--- src/test.py
+++ src/test.py
@@ -98,7 +98,6 @@ class user_input():
             print(self.buffer) # debug
             print('', '\n/ ' + self.buffer, end='') # prints the initial line
             time.sleep(0.5) # waits a second
-            dum = input()
             print('', end='\x1b[2K') # clears current
             print('\033[1A', end='\x1b[2K') # moves up and clears
             clear_string(len(self.buffer))
@@ -129,3 +128,6 @@ class user_input():
 
 
 test = user_input()
+
+while(1):
+    input()
