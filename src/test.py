6a89b02a9f19f6d85fc5c054d369702495aa48a6
wettestsock <evilsharkeater@gmail.com>
2024-04-21 00:28:21 -0400

    turned user input into a class

--- src/test.py
+++ src/test.py
@@ -11,14 +11,20 @@ class user_input():
 
     # inits everything
     def __init__(self):
-        self.user = '' # user input
-        self.quit = False # exits the program
-        self.input = True # exits the user input (can be turned on/off)d
+        self.input = '' # user input
+
+        # current variables (focus, etc)
+        self.current = {'input': True, # whether there's input to take (turn on/off when switching tabs), change only in threads
+                        'quit': False, # whether to quit or not (only when to quit the program)
+                        'window': 'main' # the current focus window
+        }
+
 
          # THREADS
         # all threads should be daemons
         self.keylog = Listener(on_press=self.on_press) # daemon on default
         self.main = threading.Thread(target=self.main_input, daemon=True)
+        self.picker = threading.Thread(target=self.picker, daemon=True)
 
 
         # THREADS
@@ -32,24 +38,31 @@ class user_input():
     def on_press(self, key:Key):
         match key:
             case Key.backspace:
-                self.user = self.user[:-1]
+                self.input = self.input[:-1]
+
+            # ALL INPUT COMMANDS
             case Key.enter:
-                match self.user:
-                    case 'quit':
-                        print('') # have to do this idk why 
-                        self.input = False
-                        self.quit = True
-                    case 'search':
-                        print('')
-                        self.input = False
+                match self.input_window:
+                    case 'main':
+                
+                        match self.input:
+                            case 'quit':
+                                print('') # have to do this idk why 
+                                self.input = False
+                                self.quit = True
+                            case 'search': 
+                                print('')
+                                self.input = False # exits the input loop
+                                self.current['window'] = 'picker' #picker window
+
     
                 print('\033[1A', end='\x1b[2K') # clears the current line
-                self.user='' # wipes the user buffer
+                self.input='' # wipes the user buffer
             case None:
                 pass
             case _: # regular letterssd
                 try:
-                    self.user += key.char
+                    self.input += key.char
                 except:
                     pass
     
@@ -64,21 +77,22 @@ class user_input():
     def main_input(self):
         print('')
         delete_line()
-        while(not self.quit):
-            if len(self.user) > 10:
-                self.user = self.user[:10]
+        while(not self.quit and self.current['window'] == 'main'):
+            if len(self.input) > 10:
+                self.input = self.input[:10]
 
-            print(self.user)
-            print('', '\n/ ' + self.user, end='') # prints the initial line
+            print(self.input)
+            print('', '\n/ ' + self.input, end='') # prints the initial line
             time.sleep(0.5) # waits a second
 
             print('', end='\x1b[2K') # clears current
             print('\033[1A', end='\x1b[2K') # moves up and clears
-            clear_string(len(self.user))
-            print('/ ' + self.user, end='') # moves cursor to the right
+            clear_string(len(self.input))
+            print('/ ' + self.input, end='') # moves cursor to the right
             print('') # prints newline
             delete_line(1) # deletes it (so lines in next iteration will start at beginning)
 
+    def picker(self):
 
 
 
