e6cbe9bdc3042ee5b85245e403c3e7bd0f7138ca
wettestsock <evilsharkeater@gmail.com>
2024-04-22 18:42:35 -0400

    nothing of note

--- src/futils.py
+++ src/futils.py
@@ -98,7 +98,7 @@ def start_player():
                                 '--disable-credential-cache ' +
                                 '--autoplay ' + 
                                 '--device-type homething ' +
-                                '-u \"'+ gl.auth_codes['user_id'] +'\" ' +
+                                '-u \"'+ gl.auth_codes['user_id'] + '\" ' +
                                 '-p \"' + gl.auth_codes['password'] + '\"',
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     
