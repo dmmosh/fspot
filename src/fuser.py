3753b32d713e943b2e982c051c09d552d8251570
wettestsock <evilsharkeater@gmail.com>
2024-04-22 18:18:57 -0400

    nothing of note

--- src/fuser.py
+++ src/fuser.py
@@ -136,7 +136,9 @@ class user_input():
             print('\n/ ' + self.buffer, end='') # prints the initial line
 
             time.sleep(0.5) # waits a second
-            
+            clear_line()
+            print('\n/ ' + self.buffer, end='') # prints the initial line
+
             move_up()
             for i in range(0,3):
                 move_up()
