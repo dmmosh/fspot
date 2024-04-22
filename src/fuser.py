3eeda8d0a878d7b1df72d40fb2881bcf3f594287
wettestsock <evilsharkeater@gmail.com>
2024-04-22 17:41:20 -0400

    nothing of note

--- src/fuser.py
+++ src/fuser.py
@@ -134,7 +134,7 @@ class user_input():
             print('', end='\x1b[2K') # clears current
             delete_line(3) # delete the amount of lines being printed above (including the terminal input)
 
-            print('\n\n\n/ ', end='')
+            print('\n\n\n/ ' + self.buffer, end='')
 
             loc = GET('me/player')
             if (loc.status_code == 200):
