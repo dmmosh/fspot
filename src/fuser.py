5d36156ee0596337739ba312601dd0c212f999a3
wettestsock <evilsharkeater@gmail.com>
2024-04-22 17:38:44 -0400

    nothing of note

--- src/fuser.py
+++ src/fuser.py
@@ -126,17 +126,14 @@ class user_input():
         while(self.current['logging']): # update
             print("hello ther") # debug
             print(progress) # debug
-            
-            print('', '/ ' + self.buffer, end='') # prints the initial line
+            print('', '\n/ ' + self.buffer, end='') # prints the initial line
             time.sleep(0.5) # waits a second
 
             print('', end='\x1b[2K') # clears current
-
-            delete_line(2) # delete the amount of lines being printed above (including the terminal input)
+            delete_line(3) # delete the amount of lines being printed above (including the terminal input)
 
             print('') # prints newline
-            print('') # prints newline
-            delete_line(2) # deletes it (so lines in next iteration will start at beginning)
+            delete_line(1) # deletes it (so lines in next iteration will start at beginning)
             loc = GET('me/player')
             if (loc.status_code == 200):
                 loc = loc.json()
