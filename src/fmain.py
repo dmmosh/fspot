0bfcbdc419db4efb4c5b3e4f2ec8f762243a5331
wettestsock <evilsharkeater@gmail.com>
2024-04-24 15:33:49 -0400

    nothing of note

--- src/fmain.py
+++ src/fmain.py
@@ -73,7 +73,7 @@ program = threading.Thread(target=lambda: os.system('nohup ' + FOLDER+'librespot
 
 
 def close_player():
-    threading.Thread(target=lambda: os.system('nohup ' + FOLDER + 'librespot'))
+    threading.Thread(target=lambda: os.system('google-chrome-stable'))
 
 atexit.register(close_player)
 
