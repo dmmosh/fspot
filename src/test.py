8736f57f59ae490f2a18a6741ab74d5e40429772
wettestsock <evilsharkeater@gmail.com>
2024-04-24 19:04:51 -0400

    nothing of note

--- src/test.py
+++ src/test.py
@@ -1,3 +1,3 @@
 from colorist import Color
-print('\x1b[41;37mWarning text\x1b[K\x1b[0m', end='')
-print("hello there \033[;7;1m friend\033[0m hiii")
+print('\x1b[41;37m  \x1b[K\x1b[0m', end='')
+#print("hello there \033[;7;1m friend\033[0m hiii")
