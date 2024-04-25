0f117002be70a3bfce9e38ec0f5258736967f89f
wettestsock <evilsharkeater@gmail.com>
2024-04-24 20:59:15 -0400

    nothing of note

--- src/futils.py
+++ src/futils.py
@@ -96,13 +96,12 @@ def loading_msg(process:threading.Thread, msg:str = 'Loading...')-> None:
     else: # TITLE 4 (123 and over)
         title = {'id': 4, 'line_num': 25, 'col_num': 123, 'len': 2609}
 
-    lines = []
-    with open(FOLDER + 'titles/title' + str(title['id']) + '.txt', 'r') as f:
-        lines = [line.rstrip('\n') for line in f]
+    title_text = open(FOLDER + 'titles/title' + str(title['id']) + '.txt', 'r')
+
+    COL = gl.term_size
+    [ print(line.center(COL)) for line in title_text.readlines()]
     
-    for line in lines:
-        print(('\x1b[39;49;1m' + line + '\x1b[K\x1b[0m'))
-    
+    title_text.close()
 
     print_msg = msg
     while(process.is_alive()):
