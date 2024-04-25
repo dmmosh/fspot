5fea9e52acade20afbc8d725c3a061025f1173db
wettestsock <evilsharkeater@gmail.com>
2024-04-24 20:45:00 -0400

    nothing of note

--- src/futils.py
+++ src/futils.py
@@ -102,7 +102,7 @@ def loading_msg(process:threading.Thread, msg:str = 'Loading...')-> None:
     cols = gl.term_size
     for line in title_text:
         print(('\x1b[39;49;1m' + line + '\x1b[K\x1b[0m').center(cols), end='')
-
+    print('')
 
 
 
