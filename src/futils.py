f5b068aac6d62c7a6ab13d3c780ae9a5adfe1bf9
wettestsock <evilsharkeater@gmail.com>
2024-04-24 20:48:54 -0400

    nothing of note

--- src/futils.py
+++ src/futils.py
@@ -102,7 +102,7 @@ def loading_msg(process:threading.Thread, msg:str = 'Loading...')-> None:
     for line in title_text:
         print(('\x1b[39;49;1m' + line + '\x1b[K\x1b[0m').center(gl.term_size), end='')
     
-
+        
     print_msg = msg
     while(process.is_alive()):
         for char in "/—\|":
