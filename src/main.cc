0e9318c14abe0b073f26acbfe9ccc9b5a7854a53
wettestsock <evilsharkeater@gmail.com>
2024-05-02 18:41:10 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -9,15 +9,15 @@ git-all && clang++ -std=c++20 src/main.cc src/utils.cc -o fspot/fplayer && ./fsp
 */
 
 int main(int argc, char* argv[]){
-    if (argc !=5) ERROR("Not enough arguments. I see what you did there.");
+    //if (argc !=5) ERROR("Not enough arguments. I see what you did there.");
     
 
-    move::clear();
-    move::up_clear(std::stoi(argv[4])); // clears the loading message
+    //move::clear();
+    //move::up_clear(std::stoi(argv[4])); // clears the loading message
 
 
     main_player player;
-    
+
 
     // PRINTS THE ARGUMENTS
     /*
