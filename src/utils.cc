b309711abd9af78e24d5ec551c3e211ecb97c629
wettestsock <evilsharkeater@gmail.com>
2024-05-02 17:02:54 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -27,15 +27,15 @@ namespace move{
 
 }
 
-void sleep(const double& sec){    
-    std::this_thread::sleep_for(std::chrono::milliseconds((int)(sec*1000)));    
-}
 
-void main_input(){
-    std::string input;
+// MAIN PLAYER CLASS
 
-    std::jthread log_thread(keylog, std::ref(input));
+main_player::main_player(){
+    this->type = true;
+    this->input = "";
 
+    std::jthread log_thread(keylog<main_player>, this);
+    
     move::down(3);
     move::up(3);
     while(input != "quit"){
@@ -58,37 +58,16 @@ void main_input(){
     move::down(3);
 }
 
-
-
-void keylog(std::string& into){
-        while(into != "quit"){
-        char get = getch();
-        switch(get){
-            case ENTER:
-                    into = "";
-            break;  
-            default:
-                into.push_back(get);
-        }
+void main_player::commands(){
+    if (this->input == "quit"){
+        this->type = false;
+        exit(0);
     }
-}
+    
+    return;
+};
 
-char getch() {
-        char buf = 0;
-        struct termios old = {0};
-        if (tcgetattr(0, &old) < 0)
-                perror("tcsetattr()");
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
-        old.c_cc[VMIN] = 1;
-        old.c_cc[VTIME] = 0;
-        if (tcsetattr(0, TCSANOW, &old) < 0)
-                perror("tcsetattr ICANON");
-        if (read(0, &buf, 1) < 0)
-                perror ("read()");
-        old.c_lflag |= ICANON;
-        old.c_lflag |= ECHO;
-        if (tcsetattr(0, TCSADRAIN, &old) < 0)
-                perror ("tcsetattr ~ICANON");
-        return (buf);
+
+void sleep(const double& sec){    
+    std::this_thread::sleep_for(std::chrono::milliseconds((int)(sec*1000)));    
 }
