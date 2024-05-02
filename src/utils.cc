8479ec9497c3ebb08b7b894e0d2f163339c30ee7
wettestsock <evilsharkeater@gmail.com>
2024-05-01 21:29:08 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -1,23 +1,37 @@
 #include "header.h"
 
 
+namespace move {
+void line()                           { std::cout << "\x1b[2K\r"; }
+void line(const std::string& newline) { std::cout << "\x1b[2K\r" <<  newline;  };
 
-void move::line()                           { std::cout << "\x1b[2K\r"; }
-void move::line(const std::string& newline) { std::cout << "\x1b[2K\r" <<  newline;  };
+void up()        { std::cout << "\x1b[1A"; };
+void up(int amt) { while(amt) { move::up(); amt--; } };
 
-void move::up()        { std::cout << "\x1b[1A"; };
-void move::up(int amt) { while(amt) { move::up(); amt--; } };
+void clear()        { move::up(); move::clear(); }
+void clear(int amt) { while(amt) { move::clear(); amt--; } }
 
-void move::clear()        { move::up(); move::clear(); }
-void move::clear(int amt) { while(amt) { move::clear(); amt--; } }
+}
 
 void sleep(const double& sec){    
     std::this_thread::sleep_for(std::chrono::milliseconds((int)(sec*1000)));    
 }
 
+
 void main_input(){
     std::string input = "fsdlkjl";
-    move::up();
+    
+    while(1){
+        std::cout << input << N << N;
+
+        std::cout<< "\n// " << input; 
+        sleep(0.2);
+        move::clear();
+        std::cout<< "// " << input; 
+
+        move::up();
+        move::clear(2);
+    }  
 }
 
 // keylogging function
