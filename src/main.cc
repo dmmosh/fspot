b2376653ea216bf8763e1fce769e38902aa456bb
wettestsock <evilsharkeater@gmail.com>
2024-05-02 15:24:33 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,8 @@
 FSPOT C++ CONVERSION
 
 COMPILE MANUALLY:
-python -m PyInstaller src/fmain.py --onefile --name fstart && cp dist/fstart fspot/
-git-all && clang++ -std=c++20 src/main.cc src/utils.cc -o dist/fspot && ./dist/fspot/
+python -m PyInstaller src/py/fmain.py --onefile --name fstart && cp dist/fstart fspot/
+git-all && clang++ -std=c++20 src/main.cc src/utils.cc -o fspot/fplayer && ./fspot/fplayer
 */
 
 int main(int argc, char* argv[]){
