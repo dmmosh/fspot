f71f10fc7387916cb2c389ca60e482fcf181835e
wettestsock <evilsharkeater@gmail.com>
2024-05-02 18:56:24 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -95,7 +95,7 @@ void players::keylog(){
 main_player::main_player(): players("", true){
 
 
-    std::jthread log_thread(main_player::keylog); //keylogging enabled
+    std::jthread log_thread(players::keylog); //keylogging enabled
     
     move::down(3);
     move::up(3);
