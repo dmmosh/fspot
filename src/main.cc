77a7ed0c57fc553e3dc59b60f4a8b926e893c4dd
wettestsock <evilsharkeater@gmail.com>
2024-05-01 21:22:32 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -9,7 +9,7 @@ git-all && clang++ -std=c++20 src/main.cc src/utils.cc -o build/fspot && ./build
 
 int main(void){
     //std::jthread main_thread(main_input, std::ref(input));
-    std::jthread main_thread(main_input);
+    std::thread main_thread(main_input);
     
 
     main_thread.join();
