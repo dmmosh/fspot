5e974f0908bceec2dbb20a0dc5fd3398539001e7
wettestsock <evilsharkeater@gmail.com>
2024-05-02 22:12:20 -0400

    nothing of note

--- src/test.cc
+++ src/test.cc
@@ -3,6 +3,6 @@
 
 int main(int argc, char** argv) {
     cpr::Response r = cpr::Get(cpr::Url{std::string("https://api.spotify.com/v1/" ) + "me/player"},
-                                    cpr::Header{{"Authorization", "Bearer " + std::string("BQBEZBwkmIqeogf0rFOJ9wswDW3vMw7Sb_xrVnXxQ74mzfKqO6ViAmEthtxVFgtf99wL7MEbh4ic_VvfIlV0G4k0ZO_EOKHxuYfEKXZYtu6bnLq_O5EVoKhTrlMj3oISS_b3EhkybLgQdu3fwyXUy8tFeE0L852aCDhIlAQQstyHrDEAyKMbM")}});
+                                    cpr::Header{{"Authorization", "Bearer " + std::string("BQC0pzTd5lO7S7-_P5gnL88cmoIMg-FcJ87m3pCGCoTJia84O5iPW1eGgI3LMEbvcK0kv7ujcmHiFNre7om25DKV-lbw_lLCeRPays_3sWYVV7fZ6Nn9piC78PFulOIOJnLkfGiz4rWDz8Gi5ZsH6kZYkxiMfjb_dO4yOXbOBUUd2V8h2e-wlY5Ds2PEmfYfEIvteKWo7gYXeHwF2WJNZpAs0MrOpquwwSII4rzEQhDhbAKQwkIfvbnR9waeaHFmceGt4hKcHg")}});
     std::cout << r.status_code;
 }
