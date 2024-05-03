6161234b446414674232845e117298af95202ce2
wettestsock <evilsharkeater@gmail.com>
2024-05-03 00:16:12 -0400

    nothing of note

--- src/header.h
+++ src/header.h
@@ -74,9 +74,34 @@ class players{
     void commands();
 
     cpr::Response GET(const std::string& into, ...);
-    cpr::Response POST(const std::string& into, ...);
-    cpr::Response PUT(const std::string& into, ...);
-    cpr::Response DELETE(const std::string& into, ...);
+
+    template<typename ... args>
+    cpr::Response GET(const std::string& into, const args&... rest) {
+        return cpr::Get(cpr::Url{BASE_URL + into},
+                            cpr::Header{{"Authorization", "Bearer " + ACCESS_TOKEN}},
+                            Output(rest...));
+    }
+
+    template<typename ... args>
+    cpr::Response POST(const std::string& into, const args&... rest) {
+        return cpr::Post(cpr::Url{BASE_URL + into},
+                            cpr::Header{{"Authorization", "Bearer " + ACCESS_TOKEN}},
+                            Output(rest...));
+    }
+
+    template<typename ... args>
+    cpr::Response PUT(const std::string& into, const args&... rest) {
+        return cpr::Put(cpr::Url{BASE_URL + into},
+                            cpr::Header{{"Authorization", "Bearer " + ACCESS_TOKEN}},
+                            Output(rest...));
+    }
+
+    template<typename ... args>
+    cpr::Response DELETE(const std::string& into, const args&... rest) {
+        return cpr::Delete(cpr::Url{BASE_URL + into},
+                            cpr::Header{{"Authorization", "Bearer " + ACCESS_TOKEN}},
+                            Output(rest...));
+    }
 
 };
 
