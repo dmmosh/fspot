2b2a76dac047137edd17b3708988eebcddc57cf9
wettestsock <evilsharkeater@gmail.com>
2024-05-07 15:47:14 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -194,12 +194,10 @@ void players::forward(int& ff_sec_prev, int& ff_sec, int& x, int& max, const boo
                 (void)cpr::Post(INTO("me/player/next"));
 
             } else {   // if it doesnt, actually go forward
-                int tmp_prog = progress;
-                progress+=max;
                 MESSAGE( "+" + std::to_string(max) + " sec...");
 
                 (void)cpr::Put(INTO("me/player/seek"),
-                            cpr::Parameters{{"position_ms", std::to_string((tmp_prog + max)*1000)}});
+                            cpr::Parameters{{"position_ms", std::to_string((progress + max)*1000)}});
             }
             
             break;
@@ -214,12 +212,10 @@ void players::forward(int& ff_sec_prev, int& ff_sec, int& x, int& max, const boo
                             cpr::Parameters{{"position_ms", "0"}});
 
             } else {   // if it doesnt, actually go forward
-                int tmp_prog = progress;
-                progress -=max;
                 MESSAGE( "-" + std::to_string(max) + " sec...");
 
                 (void)cpr::Put(INTO("me/player/seek"),
-                            cpr::Parameters{{"position_ms", std::to_string((tmp_prog - max)*1000)}});
+                            cpr::Parameters{{"position_ms", std::to_string((progress - max)*1000)}});
             }
 
 
