c153a928f93cda3f944b4dceaa9e0b524adfb62f
wettestsock <evilsharkeater@gmail.com>
2024-05-08 23:36:48 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -8,6 +8,8 @@ python -m PyInstaller src/py/fmain.py --onefile --name fstart && cp dist/fstart 
 git-all && clang++ -lcpr -std=c++20 src/main.cc src/utils.cc -o fspot/fplayer 
 
 
+git-all && clang++ -Bdynamic -lcpr -Bstatic -l:libstdc++.a -l:libc.a -std=c++20 src/main.cc src/utils.cc -o fspot/fplayer -v
+
 NOTES:
 librespot is compiled with alsa backend
 every response json variable must be stored in some temp variable
