048148fb14aa9500329b5767c2ce13412e43416f
wettestsock <evilsharkeater@gmail.com>
2024-05-10 12:45:43 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -144,7 +144,7 @@ void players::keylog(){
                     x++;
                     MINI_MESSAGE("+" + std::to_string(ff_sec)); 
                     
-                    //SLEEP(0.1);
+                    SLEEP(0.1);
                     //MESSAGE_OFF;
                 }
 
