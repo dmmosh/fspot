d88feb836623b7b616f24066ad4e6b8718b09f39
wettestsock <evilsharkeater@gmail.com>
2024-05-10 11:08:45 -0400

    nothing of note

--- src/py/fmain.py
+++ src/py/fmain.py
@@ -113,7 +113,7 @@ os.system("{}fplayer {} {} {} {} {}".format(
                            base64.b64encode(gl.auth_codes['access_token'].encode("ascii") ).decode(),
                            base64.b64encode(gl.auth_codes['refresh_token'].encode("ascii") ).decode(),
                            base64.b64encode(str(int(gl.auth_codes['expires_at'])).encode("ascii")).decode()),
-                           program.pid
+                           base64.b64encode(str(int(program.pid)).encode("ascii") ).decode()
                            )
 
 # runs after quit command
