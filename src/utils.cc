6e62cff2955b70729a8978b1a08ece6160d4f1fc
wettestsock <evilsharkeater@gmail.com>
2024-05-10 13:50:13 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -138,17 +138,15 @@ void players::keylog(){
                 //std::jthread(&players::forward, this, std::ref(ff_sec_prev), std::ref(ff_sec), std::ref(x), std::ref(max), true).detach();
                 //forward(ff_sec_prev, ff_sec, x, max, true);
                 //SLEEP(0.1);
-                ff_sec = 2;
                 while(1){
 
+                    SLEEP(0.05);
                     
                     x++;
                     ff_sec = forward_fun(x);
                     if (!ff_sec) ff_sec++;
 
-                    MINI_MESSAGE("+" + std::to_string(ff_sec)); 
 
-                    SLEEP(0.05);
 
                     MINI_MESSAGE("+" + std::to_string(ff_sec)); 
                     if (!get_char()) break;
