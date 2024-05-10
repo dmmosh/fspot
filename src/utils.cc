ce79d6d1ce1135b472d631a0d48720bc9b180ea8
wettestsock <evilsharkeater@gmail.com>
2024-05-10 13:48:23 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -138,7 +138,7 @@ void players::keylog(){
                 //std::jthread(&players::forward, this, std::ref(ff_sec_prev), std::ref(ff_sec), std::ref(x), std::ref(max), true).detach();
                 //forward(ff_sec_prev, ff_sec, x, max, true);
                 //SLEEP(0.1);
-
+                MINI_MESSAGE("ZOINKS SCOOB");
                 while(1){
 
                     
