8c8e5f8c39a3b1092348fa91abcf652e3045ec8d
wettestsock <evilsharkeater@gmail.com>
2024-05-10 11:02:11 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -19,11 +19,11 @@ every response json variable must be stored in some temp variable
 DO NOT USE EXIT(1), MEMORY LEAK WITH SMART POINTERS
 */
 
-static char* pid_encode= NULL; //pid encode ptr
+char* pid_encode= NULL; //pid encode ptr
 
 void close(){
         std::string pid = base64::decode(pid_encode);
-        system(("pkill -f "+pid+";sleep 2; pkill -9 -f "+pid).c_str());
+        system((std::string("pkill -f ")+pid+";sleep 2; pkill -9 -f "+pid).c_str());
 };
 
 int main(int argc, char* argv[]){
@@ -51,8 +51,8 @@ int main(int argc, char* argv[]){
     static int EXPIRES_AT = std::stoi(base64::decode(argv[4]));
 
     pid_encode = argv[5];  //reassigns the ptr 
-    std::cout << argv[5];   
-
+    //std::cout << argv[5];   
+    
     std::atexit(close);
 
     main_player player(ACCESS_TOKEN, REFRESH_TOKEN, EXPIRES_AT);
