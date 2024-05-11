e61a3afc4d7277bc42c81ef8e7946df22c07d440
wettestsock <evilsharkeater@gmail.com>
2024-05-10 23:19:06 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -390,8 +390,8 @@ void main_player::song_update() {
                 if (cover.load()){ // if cover is shown
                     auto album = item["album"];
                     auto images = album["images"];
-                    std::string url = images["url"];
-                    std::cout << url << NEW << NEW << NEW << NEW;
+                    //std::string url = images["url"];
+                    std::cout << images << NEW << NEW << NEW << NEW;
 
                     //std::string url = "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228";
     
