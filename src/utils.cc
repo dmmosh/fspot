aa5c4a4bc9da9bfd1ea966fbed40cc9efd0623d4
wettestsock <evilsharkeater@gmail.com>
2024-05-11 23:18:30 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -298,8 +298,11 @@ void players::commands(){
 
         if (exec("command -v icat").size()) {
             if(cover.load()) {
+                unsigned int col_size = std::min(col_update()-4, row_update()*2-14);
                 MESSAGE("Covers off!");
                 cover.store(false);
+                move::down(col_size);
+                move::up_clear(col_size);
             } else {
                 MESSAGE("Covers on!");
                 cover.store(true);
