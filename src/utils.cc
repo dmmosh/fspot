404c26da818923d009025f760da798e85b7d7526
wettestsock <evilsharkeater@gmail.com>
2024-05-12 00:24:01 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -27,14 +27,16 @@ log_thread(std::jthread(&players::keylog, this))
 };
 
 // CLASS DESTRUCTORS
-players::~players(){};
-
-main_player::~main_player(){
+players::~players(){
     if(cover.load()){
         unsigned int col_size = std::min(col_update()-4, row_update()*2-14);
         move::down(col_size);
         move::up_clear(col_size/2+3);
     }
+};
+
+main_player::~main_player(){
+    
 
     move::clear();
     move::up_clear(row_size-2);
