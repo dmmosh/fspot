ccf13ad703156f74f7bcf6cffbac177dbd0bd387
wettestsock <evilsharkeater@gmail.com>
2024-05-14 12:39:55 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -308,7 +308,7 @@ void players::commands(){
             if(cover.load()) {
 	
                 // CLEARS THE COVER ALREADY PRESENT
-                unsigned int col_size = std::min(col_update()-2, row_update()*2-12);
+                unsigned int col_size = std::min(col_update()-2, row_update()*2-10);
                 move::down(col_size);
                 move::up_clear(col_size/2+3);
                 cover.store(false);
