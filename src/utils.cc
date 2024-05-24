6e8b59599c81e785dd1c7968dab09f4eb3ccac2e
wettestsock <evilsharkeater@gmail.com>
2024-05-23 23:59:13 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -534,8 +534,8 @@ void player::connect_player(){
 
         for(const auto& device: devices["devices"]){
             if (device["name"].get<std::string>() == "fspot player"){
-                cpr::Response change = cpr::Put(INTO("me/player"), cpr::Body{ "device_ids", "[ " + device["id"].get<std::string>() + " ]"});
-                std::cout <<change.status_code << NEW ;
+                cpr::Response change = cpr::Put(INTO("me/player"), cpr::Payload{{ "device_ids", {device["id"].get<std::string>()} }});
+                std::cout << change.status_code << NEW ;
             }
         }
         std::cout << NEW << NEW << NEW << NEW;
