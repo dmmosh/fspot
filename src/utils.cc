ec4920b6750b5d05c5def6934d133a37bf17ec79
wettestsock <evilsharkeater@gmail.com>
2024-05-29 15:20:42 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -345,7 +345,7 @@ void player::volume(const bool add_substr, unsigned short& input_len){
         input_len = 0;
         MESSAGE_OFF;
     } else {
-        MESSAGE(message.substr(2, 7), 2.0);
+        MESSAGE(message.substr(2, 6), 2.0);
     }
 };
 
